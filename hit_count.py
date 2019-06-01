aabd69f12fb73657076eb051f911a1e82c47fcb4
yea_jin <jekr16@naver.com>
2019-06-01 13:47:17 +0900

    create "hit_count.py" Yeajin

---
+++ hit_count.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from bs4 import BeautifulSoup
+#import sys
+
+
+#sys.setdefaultencoding('utf-8')
+
+class hit_count_scrap:
+    def __init__(self, soup):
+
+        if(soup.select('#contents_area > div.view2_pic > div.view_cate.st2 > div > span')):
+            self.hit_count = soup.select('#contents_area > div.view2_pic > div.view_cate.st2 > div > span')
+            for i in range(len(self.hit_count)):
+                self.hit_count = self.hit_count[i].text
+                print(self.hit_count)
+
+            print('=' * 20)
+
+    def hit_count_rt(self):
+        return self.hit_count
